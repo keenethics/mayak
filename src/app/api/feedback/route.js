8a7e2e6aacebaae7aeb061e862cbe0af42e1f236
Vladislav <dmitrenkov1110@gmail.com>
2024-02-01 12:00:27 +0200

    [MID-10] added /api/feedback route and server-side validation

---
+++ src/app/api/feedback/route.js
@@ -0,0 +1,52 @@
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegex = /^\+380\d{9}$/;
+
+const Feedback = z
+  .object({
+    name: z.string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    }),
+    phone: z
+      .string({
+        required_error: "Phone number is required",
+        invalid_type_error: "Phone number must be a string",
+      })
+      .refine((val) => phoneRegex.test(val), {
+        message: "Please, enter phone number in format +380XXXXXXXXX",
+      }),
+    callAgain: z.boolean({
+      required_error: "callAgain is required",
+      invalid_type_error: "callAgain must be a boolean",
+    }),
+    email: z.string().refine((val) => emailRegex.test(val), {
+      message: "Please, enter valid email",
+    }),
+    message: z
+      .string({
+        required_error: "Message is required",
+        invalid_type_error: "Message must be a string",
+      })
+      .max(320, { message: "Message must not be longer than 320 characters" }),
+  })
+  .partial({ email: true });
+
+export async function POST(request) {
+  try {
+    const feedbackData = await request.json();
+    const validatedFeedback = Feedback.safeParse(feedbackData);
+    if (!validatedFeedback.success) {
+      return NextResponse.json({ errors: validatedFeedback.error.flatten().fieldErrors }, { status: 400 });
+    } else {
+      // const feedback = await prisma.feedback.create({ data: feedbackData });
+      return NextResponse.json(feedbackData);
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
+}
