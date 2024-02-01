4f6a1c6bbec77a4d63d61cb3c4a18adaa849260c
Vladislav <dmitrenkov1110@gmail.com>
2024-02-02 00:58:59 +0200

    [MID-10] added POST request handler to /api/feedback and validation

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -1,37 +1,43 @@
-import { prisma } from "@/lib/db";
-import { NextResponse } from "next/server";
-import { z } from "zod";
+import { NextResponse } from 'next/server';
+import { z } from 'zod';
+import { prisma } from '../../../lib/db';
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const phoneRegex = /^\+380\d{9}$/;
 
 const Feedback = z
   .object({
-    name: z.string({
-      required_error: "Name is required",
-      invalid_type_error: "Name must be a string",
-    }),
+    name: z
+      .string({
+        required_error: 'Name is required',
+        invalid_type_error: 'Name must be a string',
+      })
+      .max(128, {
+        message: 'Name must not be longer than 128 characters',
+      }),
     phone: z
       .string({
-        required_error: "Phone number is required",
-        invalid_type_error: "Phone number must be a string",
+        required_error: 'Phone number is required',
+        invalid_type_error: 'Phone number must be a string',
       })
-      .refine((val) => phoneRegex.test(val), {
-        message: "Please, enter phone number in format +380XXXXXXXXX",
+      .refine(val => phoneRegex.test(val), {
+        message: 'Please, enter phone number in format +380XXXXXXXXX',
       }),
     callAgain: z.boolean({
-      required_error: "callAgain is required",
-      invalid_type_error: "callAgain must be a boolean",
+      required_error: 'callAgain is required',
+      invalid_type_error: 'callAgain must be a boolean',
     }),
-    email: z.string().refine((val) => emailRegex.test(val), {
-      message: "Please, enter valid email",
-    }),
+    email: z
+      .string()
+      .refine(val => emailRegex.test(val) && val.length <= 254, {
+        message: 'Please, enter valid email',
+      }),
     message: z
       .string({
-        required_error: "Message is required",
-        invalid_type_error: "Message must be a string",
+        required_error: 'Message is required',
+        invalid_type_error: 'Message must be a string',
       })
-      .max(320, { message: "Message must not be longer than 320 characters" }),
+      .max(320, { message: 'Message must not be longer than 320 characters' }),
   })
   .partial({ email: true });
 
@@ -40,13 +46,14 @@ export async function POST(request) {
     const feedbackData = await request.json();
     const validatedFeedback = Feedback.safeParse(feedbackData);
     if (!validatedFeedback.success) {
-      return NextResponse.json({ errors: validatedFeedback.error.flatten().fieldErrors }, { status: 400 });
-    } else {
-      // const feedback = await prisma.feedback.create({ data: feedbackData });
-      return NextResponse.json(feedbackData);
+      return NextResponse.json(
+        { errors: validatedFeedback.error.flatten().fieldErrors },
+        { status: 400 },
+      );
     }
+    const feedback = await prisma.feedback.create({ data: feedbackData });
+    return NextResponse.json(feedback);
   } catch (err) {
-    console.log(err);
     return NextResponse.json({ error: err }, { status: 500 });
   }
 }
