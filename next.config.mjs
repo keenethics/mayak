151078ea2089bd795d48be9296d82bec248ea2d9
watchlar2000 <sergii.koch@gmail.com>
2024-02-01 22:23:25 +0200

    [MID-20] remove redundant compiler from nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [join(__dirname, 'styles')],
-  },
-  compiler: {
-    styledComponents: true,
-  },
+    includePaths: [join(__dirname, "styles")]
+  }
 };
 
 export default nextConfig;
