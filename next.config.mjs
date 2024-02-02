62b4df99da86b9432c15cf72dbae9804e7612a11
Vladislav <dmitrenkov1110@gmail.com>
2024-02-02 12:53:40 +0200

    [MID-10] fixed linter errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [join(__dirname, "styles")],
+    includePaths: [join(__dirname, 'styles')],
   },
 };
 
