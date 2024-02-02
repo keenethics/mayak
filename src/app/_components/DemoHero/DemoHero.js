15f385837a1da8ed03376ee5b4b05b6f575f5b0f
watchlar2000 <sergii.koch@gmail.com>
2024-02-02 17:58:30 +0200

    [MID-20] fix lint errors

--- src/app/_components/DemoHero/DemoHero.js
+++ src/app/_components/DemoHero/DemoHero.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 export default function DemoHero() {
   return (
     <section className="bg-secondary-100 p-5">
       <h1 className="text-h1 font-bold text-primary-600">
-        Шукай пункти психологічної підтримки у місті{" "}
+        Шукай пункти психологічної підтримки у місті{' '}
         <span className="p-4 pt-2 text-secondary-500">Львові</span>
       </h1>
       <p className="mt-5 text-p1 font-bold">
