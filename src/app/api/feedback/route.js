c2d34017c713bd17934c9b5d17c776af71d70423
Vladislav <dmitrenkov1110@gmail.com>
2024-02-04 16:00:27 +0200

    [MID-10] changed imports to @

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '../../../lib/db';
-import { Feedback } from '../../../lib/validationSchemas';
+import { prisma } from '@/lib/db';
+import { Feedback } from '@/lib/validationSchemas';
 
 export async function POST(request) {
   try {
