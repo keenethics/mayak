1d2117b5990e30ff12378b8e3b19cd8cdaae5bff
Mykhailo Pasternak <mikhailo@modulize.ai@gmail.com>
2024-02-05 20:19:05 +0200

    [MID-28] add ClientPortal and Modal components

--- src/app/_components/Header/Header.js
+++ src/app/_components/Header/Header.js
@@ -1,9 +1,29 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Modal from '../Modal';
 
 export default function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => {
+    setIsModalOpen(prevState => !prevState);
+  };
   return (
-    <header className="bg-primary-100 p-5 text-h1 text-primary-700">
-      Header
+    <header className="flex justify-between bg-primary-100">
+      <h1 className="p-3 text-h1 text-primary-700"> Header </h1>
+      <button
+        onClick={() => toggleModal()}
+        className="rounded-lg text-primary-500 outline"
+      >
+        Зворотній зв`язок
+      </button>
+
+      <Modal isOpen={isModalOpen} onClose={toggleModal}>
+        <div className="rounded-md bg-primary-300 p-4 shadow-md">
+          <h2>This is a Modal</h2>
+          <p>Modal content goes here.</p>
+        </div>
+      </Modal>
     </header>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
         <Header />
         <QueryContext>{children}</QueryContext>
         <Footer />
+        <div id="modal-root" />
       </body>
     </html>
   );
