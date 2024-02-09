9933d7a610433733d19d233b9a80183573e9a333
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-02-09 20:32:20 +0200

    [MID-37] adapted footer to mobile

--- src/app/_components/Footer.js
+++ src/app/_components/Footer.js
@@ -10,8 +10,11 @@ export default function Footer() {
   const { links } = siteNav;
 
   // Basic styles
-  const flexBetween = 'inline-flex flex-row items-center justify-between';
+  const flexBetweenMd = 'md:inline-flex md:flex-row md:items-center md:justify-between';
   const flexCenter = 'inline-flex flex-row items-center justify-center';
+  const flexCenterMd = 'md:inline-flex md:flex-row md:items-center md:justify-center';
+  const flexColCenter = 'flex flex-col items-center';
+  const flexColRevCenter = 'flex flex-col-reverse items-center';
   const basicLink = 'no-underline list-none cursor-pointer';
   const iconColors = 'text-other-white hover:text-primary-400';
   const transition = 'transition duration-200 ease-in-out';
@@ -22,18 +25,20 @@ export default function Footer() {
   const outlinedColors = 'border-other-white text-other-white';
 
   return (
-    <footer className={cn('text-white flex w-full flex-col bg-primary-800 px-20 py-12')}>
-      <div className={cn(flexBetween)}>
+    <footer
+      className={cn('text-white relative flex w-full flex-col overflow-hidden bg-primary-800 p-4 md:px-20 md:py-12')}
+    >
+      <div className={cn(flexColCenter, flexBetweenMd, 'gap-4')}>
         <Link href="/" aria-label="Reload main page on logo click" className={cn(basicLink)}>
           <FooterImg
             alt="Footer logo image"
             aria-label="Footer logo image"
             priority="true"
-            className={cn('h-[74px] w-[129px]')}
+            className={cn('flex-no-shrink fill-current h-[36px] w-[66px] md:h-[74px] md:w-[129px]')}
           />
         </Link>
-        <div className={cn(flexCenter, 'gap-6 text-other-white')}>
-          <p className={cn('text-p1 font-bold')}>Ставай нашим партнером</p>
+        <div className={cn(flexColCenter, flexCenterMd, 'gap-2 py-3 text-other-white md:gap-6')}>
+          <p className={cn('text-p4 font-bold md:text-p1')}>Ставай нашим партнером</p>
           <OutlinedBtn
             className={cn(footerBtnHover, footerBtnFocus, footerBtnActive, footerBtnBorder, outlinedColors, 'text-p4')}
             aria-label="Click to fill application form"
@@ -42,17 +47,18 @@ export default function Footer() {
           </OutlinedBtn>
         </div>
       </div>
-      <div className={cn('my-12 h-[1px] w-full bg-gray-300')}></div>
-      <div className={cn(flexBetween, 'text-other-white')}>
-        <p className={cn('text-p4 font-normal ')}>Маяк @ 2024</p>
-        <div className={cn(flexCenter, 'gap-4')}>
-          <p className={cn('text-p2 font-medium ')}>Слідкуй за нами в соцмережах</p>
+      <div className={cn('relative left-[-10%] my-3 h-[1px] w-[120%] bg-gray-300 md:static md:my-12 md:w-full')}></div>
+      <div className={cn(flexColRevCenter, flexBetweenMd, 'gap-4 text-other-white')}>
+        <p className={cn('text-[0.75rem] font-normal leading-5 md:text-p4 ')}>Маяк @ 2024</p>
+        <div className={cn(flexCenter, 'gap-5 md:gap-4')}>
+          <p className={cn('hidden md:inline', 'text-p2 font-medium')}>Слідкуй за нами в соцмережах</p>
+          <p className={cn('inline md:hidden', 'text-p4')}>Наші соціальні мережі:</p>
           <SocialLink
             role="list"
             status="footerSocials"
             links={links}
             className={cn(basicLink, transition, iconColors, 'hover:color-primary-500 gap-4 hover:text-primary-500')}
-          ></SocialLink>
+          />
         </div>
       </div>
     </footer>
