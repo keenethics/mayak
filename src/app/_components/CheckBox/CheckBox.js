ab1e3909024305e3dd3f01d240626457e9d11fa4
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-02-09 18:38:30 +0200

    [MID-25] use tickicon from icons folder

--- src/app/_components/CheckBox/CheckBox.js
+++ src/app/_components/CheckBox/CheckBox.js
@@ -4,6 +4,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { cn } from '@/utils/cn';
 import { variants } from './styles';
+import { TickIcon } from '../Icons';
 
 export function CheckBox({
   value,
@@ -49,43 +50,14 @@ export function CheckBox({
         htmlFor={id}
       >
         {checked && (
-          <Tick
-            styles={cn(
-              variant.tick.position,
-              variant.tick.base,
-              disabled && variant.tick.disabled,
-            )}
-          />
+          <TickIcon className={cn(variant.tick.position, variant.tick.base, disabled && variant.tick.disabled)} />
         )}
         {text && (
-          <div
-            className={cn(
-              variant.textContainer.base,
-              variant.textContainer.position,
-            )}
-          >
+          <div className={cn(variant.textContainer.base, variant.textContainer.position)}>
             {text ? (
               <>
-                {text && (
-                  <p
-                    className={cn(
-                      variant.text.base,
-                      disabled && variant.text.disabled,
-                    )}
-                  >
-                    {text}
-                  </p>
-                )}
-                {subText && (
-                  <p
-                    className={cn(
-                      variant.subText.base,
-                      disabled && variant.subText.disabled,
-                    )}
-                  >
-                    {subText}
-                  </p>
-                )}
+                {text && <p className={cn(variant.text.base, disabled && variant.text.disabled)}>{text}</p>}
+                {subText && <p className={cn(variant.subText.base, disabled && variant.subText.disabled)}>{subText}</p>}
               </>
             ) : (
               { children }
@@ -97,29 +69,6 @@ export function CheckBox({
   );
 }
 
-function Tick({ styles }) {
-  return (
-    <svg
-      className={styles}
-      xmlns="http://www.w3.org/2000/svg"
-      width="12"
-      height="9"
-      viewBox="0 0 12 9"
-      fill="none"
-    >
-      <path
-        d="M10.6663 1.5L4.24967 7.91667L1.33301 5"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
-Tick.propTypes = {
-  styles: PropTypes.string,
-};
 CheckBox.propTypes = {
   value: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
