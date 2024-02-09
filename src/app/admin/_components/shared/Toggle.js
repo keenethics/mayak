b04424bc7451b4d16169a52d649660b6e9f24a6f
watchlar2000 <sergii.koch@gmail.com>
2024-02-09 10:54:19 +0200

    [MID-22] change toggle prop name

--- src/app/admin/_components/shared/Toggle.js
+++ src/app/admin/_components/shared/Toggle.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Toggle = ({ onChange, formMode }) => (
+const Toggle = ({ onChange, caption }) => (
   <div className="m-4">
     <label className="relative inline-flex cursor-pointer items-center">
       <input type="checkbox" onChange={onChange} className="peer sr-only" />
       <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-other-white after:transition-all after:content-[''] peer-checked:bg-primary-600 peer-checked:after:translate-x-full peer-checked:after:border-other-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-400 rtl:peer-checked:after:-translate-x-full dark:border-gray-800 dark:bg-gray-900 dark:peer-focus:ring-primary-800"></div>
       <span className="text-sm ms-3 font-medium text-gray-900 dark:text-gray-300">
-        <p className="font-bold text-primary-700">{formMode}</p>
+        <p className="font-bold text-primary-700">{caption}</p>
       </span>
     </label>
   </div>
@@ -15,7 +15,7 @@ const Toggle = ({ onChange, formMode }) => (
 
 Toggle.propTypes = {
   onChange: PropTypes.func,
-  formMode: PropTypes.string,
+  caption: PropTypes.string,
 };
 
 export { Toggle };
