b69119e566f5a8dbcfc7c5d90714296b89de1e4c
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-02-12 22:19:23 +0200

    [MID-32] update hooks log messages

--- .husky/commit-msg.js
+++ .husky/commit-msg.js
@@ -7,8 +7,8 @@ const testRegExp = /^\[MID-[0-9]+\] /g;
 const result = testRegExp.exec(commitMsg);
 if (result === null) {
   console.log('Commit message should be of the form: "[MID-<ticket number>] <commit message text>"');
-  console.log('Example: "[MID-123] add this cool feature"');
-  console.log(`Provided commit message ${commitMsg}`);
+  console.log('Example:  "[MID-123] add this cool feature"');
+  console.log(`Provided: "${commitMsg}"`);
   process.exit(1);
 }
 process.exit(0);
--- .husky/pre-push.js
+++ .husky/pre-push.js
@@ -5,8 +5,8 @@ const testRegExp = /^MID-[0-9]+_([a-zA-Z0-9]+-?)+[^-]$/g;
 const result = testRegExp.exec(branchName);
 if (result === null) {
   console.log('Branch name should be of the form: "MID-<ticket number>_<branch-name-text-in-kebab-case>"');
-  console.log('Example: "MID-123_add-this-cool-feature"');
-  console.log(`Provided branch name ${branchName}`);
+  console.log('Example:  "MID-123_add-this-cool-feature"');
+  console.log(`Provided: "${branchName}"`);
   process.exit(1);
 }
 process.exit(0);
