f314a77e27e773775494a510c00381a2a9f616e5
Mykhailo Pasternak <mikhailo@modulize.ai@gmail.com>
2024-02-14 19:36:40 +0200

    [MID-34] add adaptive styles for not-found page (md, lg, xl)

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import Link from "next/link";
-import { cn } from '@utils/cn';
 import NotFound from '@icons/notFound.png';
 import Image from 'next/image';
 
 const NotFoundPage = () => {
 
-  const basicBtnHover = 'hover:bg-primary-200 focus:border-primary-600';
-  const basicBtnFocus = 'focus:text-primary-600 focus:bg-primary-200';
-  const basicBtnActive = 'active:bg-primary-200 active:text-primary-600';
-
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="text-center">
+    <div className="flex justify-center mb-10 lg:mb-20 xl:mb-28">
+      <div className='flex-col text-center'>
+        <Image src={NotFound} alt="Not Found image" aria-label="Not Found image" width="430px" height="376px" className='my-0 lg:my-[16px] xl:my-[30px]' />
 
-        <Image src={NotFound} alt="Not Found image" aria-label="Not Found image" width="430px" height="376px" />
-
-        <div className="whitespace-pre text-primary-700 text-[18px] lg:text-[28px]">
+        <div className="whitespace-pre-line text-primary-700 text-[18px] lg:text-[28px] my-[26px] lg:my-[30px] xl:my-[45px]">
           Error
           <div className="text-h4 lg:text-h2 font-bold">404</div>
           Not found
         </div>
 
-        <Link href={"/"} className="text-sm font-bold block bg-primary-300 w-full py-2.5 ">Повернутись на головну</Link>
-
+        <Link href={"/"} aria-label="Click to back to the home page" className={`align-middle bg-primary-500 text-other-white hover:bg-primary-400 focus:outline-none focus:bg-primary-600 active:bg-primary-700 text-sm font-bold px-6 py-3 rounded-[100px] transition-all duration-300 text-p3 `}>
+          Повернутись на головну
+        </Link>
       </div>
+
     </div >
   );
 };
