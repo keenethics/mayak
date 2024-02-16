0a79070e774bf504eea541243df79b62c6135b50
watchlar2000 <sergii.koch@gmail.com>
2024-02-16 09:29:12 +0200

    [MID-22] update schema

--- src/lib/validationSchemas/specialistSchema.js
+++ src/lib/validationSchemas/specialistSchema.js
@@ -10,23 +10,19 @@ const MESSAGES = {
 const zString = z
   .string({
     required_error: MESSAGES.requiredField,
-    invalid_type_error: MESSAGES.requiredField,
+    invalid_type_error: MESSAGES.unacceptableValue,
   })
   .trim();
 
-const zStringWithMinMax = zString
-  .min(2, {
-    message: 'Поле повинно мати не менше двох символів',
-  })
-  .max(128, {
-    message: 'Поле не повинно перевищувати 128 символів',
-  });
+const zStringWithMax = zString.max(128, {
+  message: 'Поле не повинно перевищувати 128 символів',
+});
 
 const zStringArray = zString.array().min(1, {
   message: MESSAGES.requiredField,
 });
 
-const yearsOfExperience = z
+const zYearsOfExperience = z
   .number({
     required_error: MESSAGES.requiredField,
     invalid_type_error: MESSAGES.unacceptableValue,
@@ -34,35 +30,34 @@ const yearsOfExperience = z
   .nonnegative()
   .nullish();
 
-const zPlacesOfWork = z.array(
-  z
-    .object({
-      fullAddress: zStringWithMinMax,
-      nameOfClinic: z.string().nullish(),
-      district: zString,
-    })
-    .default([]),
+const zPlacesOfWorkSchema = z.array(
+  z.object({
+    fullAddress: zStringWithMax,
+    nameOfClinic: z.string().nullish(),
+    district: zString,
+  }),
+  // .default([]),
 );
 
 const defaultProps = z.object({
-  lastName: zStringWithMinMax,
-  firstName: zStringWithMinMax,
+  lastName: zStringWithMax,
+  firstName: zStringWithMax,
   specializations: zStringArray,
 });
 
 const restProps = z.object({
   isActive: z.boolean().optional(),
-  surname: zStringWithMinMax.nullish(),
+  surname: zStringWithMax.nullish(),
   gender: zString.refine(val => Object.values(Gender).includes(val), {
     message: MESSAGES.unacceptableValue,
   }),
-  yearsOfExperience,
+  yearsOfExperience: zYearsOfExperience,
   formatOfWork: zString.refine(val => Object.values(FormatOfWork).includes(val), {
     message: MESSAGES.unacceptableValue,
   }),
   therapies: zStringArray,
   isFreeReception: z.boolean(),
-  description: zString.trim().nullish(),
+  description: zString.nullish(),
   phone: zString
     .refine(val => PHONE_REGEX.test(val), {
       message: 'Введіть номер телефона у форматі +380XXXXXXXXX',
@@ -70,17 +65,17 @@ const restProps = z.object({
     .nullish(),
   email: zString.email().nullish(),
   website: zString.url().nullish(),
+  placesOfWork: zPlacesOfWorkSchema.nullish(),
 });
 
 const activeSpecialistSchema = restProps.extend({
   isActive: z.literal(true),
-  placesOfWork: zPlacesOfWork,
+  placesOfWork: zPlacesOfWorkSchema,
 });
 
 const draftSpecialistSchema = restProps.partial().extend({
   isActive: z.literal(false),
-  placesOfWork: zPlacesOfWork.optional(),
-  yearsOfExperience: yearsOfExperience.nullish(),
+  placesOfWork: zPlacesOfWorkSchema.nullish(),
 });
 
 const specialistSchemaUnion = z.discriminatedUnion('isActive', [activeSpecialistSchema, draftSpecialistSchema]);
