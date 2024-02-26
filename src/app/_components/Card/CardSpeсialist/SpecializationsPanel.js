8880c20d976acb122c522ea7a2023530bcc9c146
BigTako <libert761@gmail.com>
2024-02-26 17:27:29 +0200

    [MID-61] extracted truncator to separate component

--- src/app/_components/Card/CardSpeсialist/SpecializationsPanel.js
+++ src/app/_components/Card/CardSpeсialist/SpecializationsPanel.js
@@ -7,7 +7,7 @@ import { Dot } from '@icons/index';
 import { Show as ShowHint, Window as HintWindow } from '../../Hint';
 import { Caption } from '../../Typography';
 
-export function SpecializationsPanel({ specialistId, specializations }) {
+function ListTruncator({ id, items, ellipsis = '...', itemRender, tooltipItemRender }) {
   const wrapperRef = useRef(null);
   const containerRef = useRef(null);
   const [overflown, setOverflown] = useState(false);
@@ -23,38 +23,59 @@ export function SpecializationsPanel({ specialistId, specializations }) {
   }, [wrapperRef, containerRef]);
 
   return (
-    <div id={`specializations-of-${specialistId}`} ref={containerRef} className="relative w-full">
-      <span id={`wrapper-of-${specialistId}`} className="inline-flex items-center gap-[8px] pr-[50px]" ref={wrapperRef}>
-        {specializations.map((specialization, index) => (
-          <>
-            <Caption key={index} className="whitespace-nowrap text-start font-bold text-gray-600 lg:text-p4">
-              {specialization}
-            </Caption>
-            {index !== specializations.length - 1 && <Dot />}
-          </>
-        ))}
+    <div id={`specializations-of-${id}`} ref={containerRef} className="relative w-full">
+      <span
+        id={`wrapper-of-${id}`}
+        className="inline-flex items-center gap-[8px] overflow-hidden pr-[50px]"
+        ref={wrapperRef}
+      >
+        {items.map(itemRender)}
       </span>
       {overflown && (
         <>
-          <ShowHint opens={`hint-for-${specialistId}`}>
-            <span className="absolute right-0 top-0 z-[13] h-full w-[30px] cursor-pointer bg-gradient-to-l from-other-white from-[60%] text-end text-gray-600">
-              &nbsp;...&nbsp;
+          <ShowHint opens={`hint-for-${id}`}>
+            <span className="absolute right-0 top-0 z-[13] h-full w-[80px] cursor-pointer bg-gradient-to-l from-other-white from-[30%] text-end font-bold text-gray-600">
+              &nbsp;{ellipsis}&nbsp;
             </span>
           </ShowHint>
-          <HintWindow name={`hint-for-${specialistId}`} className="right-0 top-[20px] z-[99999]">
+          <HintWindow name={`hint-for-${id}`} className="right-0 top-[20px] z-[99999]">
             <div className="flex flex-col gap-[10px] rounded-[4px] px-[8px] py-[4px] shadow-[0_2px_8px_0px_rgba(192,191,206,0.50)]">
-              {specializations.map((specialization, index) => (
-                <>
-                  <div key={index} className="text-center text-c2 text-gray-900">
-                    {specialization}
-                  </div>
-                </>
-              ))}
+              {items.map(tooltipItemRender)}
             </div>
           </HintWindow>
         </>
       )}
     </div>
+  );
+}
+
+ListTruncator.propTypes = {
+  id: P.string.isRequired,
+  items: P.arrayOf(P.string).isRequired,
+  itemRender: P.func.isRequired,
+  tooltipItemRender: P.func.isRequired,
+  ellipsis: P.string,
+};
+
+export function SpecializationsPanel({ specialistId, specializations }) {
+  return (
+    <ListTruncator
+      id={specialistId}
+      items={specializations}
+      itemRender={(specialization, index) => (
+        <>
+          <Caption key={index} className="whitespace-nowrap text-start font-bold text-gray-600 lg:text-p4">
+            {specialization}
+          </Caption>
+          {index !== specializations.length - 1 && <Dot />}
+        </>
+      )}
+      tooltipItemRender={(specialization, index) => (
+        <div key={index} className="text-center text-c2 text-gray-900">
+          {specialization}
+        </div>
+      )}
+    />
   );
 }
 
