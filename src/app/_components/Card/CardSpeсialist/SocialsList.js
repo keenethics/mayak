8e7bfb97d0886fcba9e85d0fdc6fa5d39351815e
watchlar2000 <sergii.koch@gmail.com>
2024-02-27 10:09:43 +0200

    [MID-61] fix tooltip in SocialsList

--- src/app/_components/Card/CardSpeсialist/SocialsList.js
+++ src/app/_components/Card/CardSpeсialist/SocialsList.js
@@ -1,25 +1,15 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import P from 'prop-types';
 import { specialistSocialsPropType } from '@/app/_components/Card/CardSpeсialist/prop-types';
 import { TooltipCustom } from '@/app/_components/TooltipCustom';
-import { useLineClamp } from '@/app/_hooks';
-import { cn } from '@/utils/cn';
 
 export function SocialsList({ socials }) {
-  const linkRef = useRef(null);
-  const isLinkLineClamped = useLineClamp(linkRef);
   return (
     <div className="hidden gap-[12px] lg:flex lg:flex-row lg:items-center lg:justify-center">
       {socials?.map((item, idx) =>
         item.href !== null ? (
-          <TooltipCustom
-            key={idx}
-            textToDisplay={item.href}
-            show={isLinkLineClamped}
-            className={cn('bg-primary-900 text-primary-700')}
-          >
+          <TooltipCustom key={idx} textToDisplay={item.href} show={true}>
             <a
-              ref={linkRef}
               target="_blank"
               rel="noopener noreferrer"
               className="cursor-pointer"
--- src/app/_components/TooltipCustom.js
+++ src/app/_components/TooltipCustom.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { cn } from '@/utils/cn';
 
-export function TooltipCustom({ children, textToDisplay, show = false }) {
+export function TooltipCustom({ children, textToDisplay, show = false, className }) {
   const hasTooltip = show ? 'has-tooltip' : '';
   return (
     <div className={hasTooltip}>
-      <span className="tooltip -mt-7 rounded bg-primary-700 p-1 text-c3 font-bold text-other-white shadow-lg">
+      <span
+        className={cn(
+          'tooltip -mt-7 rounded bg-primary-700 p-1 text-c3 font-bold text-other-white shadow-lg',
+          className,
+        )}
+      >
         {textToDisplay}
       </span>
       {children}
@@ -17,4 +23,5 @@ TooltipCustom.propTypes = {
   children: PropTypes.node,
   show: PropTypes.bool,
   textToDisplay: PropTypes.string,
+  className: PropTypes.string,
 };
