2f5fa712f001ac6b551d12a1aeae4e90c348e505
Mykhailo Pasternak <mikhailo@modulize.ai@gmail.com>
2024-03-01 14:44:33 +0200

    [MID-57] lint style for TextInputField

--- src/app/_components/InputFields/TextInputField.js
+++ src/app/_components/InputFields/TextInputField.js
@@ -45,7 +45,8 @@ export function TextInputField({
         {error && <InputErrorIcon className={cn(variant.errorIcon.base)} />}
       </div>
 
-      <label className={cn(variant.label.base, variant.label.stateful, absoluteLabel, error && variant.label.error)} htmlFor={id}>
+      <label className={cn(variant.label.base, variant.label.stateful, absoluteLabel, error && variant.label.error)}
+        htmlFor={id}>
         {placeholder}
       </label>
     </div>
