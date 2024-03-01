5576f42c20d79e8b10335c652446942a0066fd24
Mykhailo Pasternak <mikhailo@modulize.ai@gmail.com>
2024-03-01 14:32:56 +0200

    [MID-57] rename error message in sendFeedbackSchema

--- src/lib/validationSchemas/sendFeedbackSchema.js
+++ src/lib/validationSchemas/sendFeedbackSchema.js
@@ -3,15 +3,15 @@ import { PHONE_REGEX } from '../consts';
 import { minMaxString } from './utils';
 
 const SendFeedback = z.object({
-  name: minMaxString(1, 128, 'Name'),
+  name: minMaxString(1, 128, 'Вкажіть Ваше Імʼя'),
   phone: z
     .string({
-      required_error: 'Phone number is required',
+      required_error: 'Мобільний телефон є обовʼязковим',
       invalid_type_error: 'Phone number must be a string',
     })
     .trim()
     .refine(val => PHONE_REGEX.test(val), {
-      message: 'Please, enter phone number in format +380XXXXXXXXX',
+      message: 'Будь ласка введіть номер у форматі +380XXXXXXXXX',
     }),
   callMe: z.boolean({
     required_error: 'callMe is required',
