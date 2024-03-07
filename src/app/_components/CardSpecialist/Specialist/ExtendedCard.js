b236541c9c34c29432a3522e682368682a8e7c42
watchlar2000 <sergii.koch@gmail.com>
2024-03-07 14:42:57 +0200

    [MID-61] fix extended card styles

--- src/app/_components/CardSpecialist/Specialist/ExtendedCard.js
+++ src/app/_components/CardSpecialist/Specialist/ExtendedCard.js
@@ -5,7 +5,7 @@ import { specialistPropType } from '@/app/_components/CardSpecialist/prop-types'
 
 export function CardSpecialistExtended({ specialist, className }) {
   return (
-    <CardModalWrapper className="mt-[40px] lg:mt-0" key={specialist.id}>
+    <CardModalWrapper className="mt-[40px] lg:mt-0 w-full lg:w-[1080px] max-w-[1080px]" key={specialist.id}>
       <CardSpecialist specialist={specialist} className={className} extended />
     </CardModalWrapper>
   );
