133f92923d9fb7ee38744b18abdf95a90e445dc2
Olexander Popov <alex.popov.tech@gmail.com>
2024-03-15 18:01:09 +0200

    [MID-49] refactor icon re-exports

--- public/assets/icons/index.js
+++ public/assets/icons/index.js
@@ -1,59 +1,28 @@
-import Site from '@icons/site.svg';
-import Clock from '@icons/clock.svg';
-import HospitalSVG from '@icons/hospital.svg';
-import HospitalLogo from '@icons/hospitalIMG.svg';
-import IDCard from '@icons/id-card.svg';
-import Mail from '@icons/mail.svg';
-import MedAttention from '@icons/medical-attention.svg';
-import MedCare from '@icons/medical-care.svg';
-import OnlineMeeting from '@icons/online-meeting.svg';
-import Phone from '@icons/phone.svg';
-import Price from '@icons/price.svg';
-import Female from '@icons/female.svg';
-import Male from '@icons/male.svg';
-import Dot from '@icons/card-dot.svg';
-import BlueFb from '@icons/specialist-facebook.svg';
-import BlueInsta from '@icons/specialist-instagram.svg';
-import BlueTikTok from '@icons/specialist-tiktok.svg';
-import BlueYoutube from '@icons/specialist-youtube.svg';
-import BlueLinkedin from '@icons/specialist-linkedin.svg';
-import BlueViber from '@icons/specialist-viber.svg';
-import BlueTelegram from '@icons/specialist-telegram.svg';
-import Instagram from '@icons/instagram.svg';
-import Facebook from '@icons/facebook.svg';
-import WhiteLogo from '@icons/whiteLogo.svg';
-import CloseIcon from '@icons/closeIcon.svg';
-import HeaderCloseIcon from '@icons/headerCloseIcon.svg';
-import BurgerIcon from '@icons/burger.svg';
-import Logo from '@icons/logo.svg';
-
-export {
-  Logo,
-  Site,
-  Clock,
-  HospitalSVG,
-  HospitalLogo,
-  IDCard,
-  Mail,
-  MedAttention,
-  MedCare,
-  OnlineMeeting,
-  Phone,
-  Price,
-  Female,
-  Male,
-  Dot,
-  BlueFb,
-  BlueInsta,
-  BlueTikTok,
-  BlueYoutube,
-  BlueLinkedin,
-  BlueViber,
-  BlueTelegram,
-  Instagram,
-  Facebook,
-  WhiteLogo,
-  CloseIcon,
-  HeaderCloseIcon,
-  BurgerIcon,
-};
+export { default as Site } from '@icons/site.svg';
+export { default as Clock } from '@icons/clock.svg';
+export { default as HospitalSVG } from '@icons/hospital.svg';
+export { default as HospitalLogo } from '@icons/hospitalIMG.svg';
+export { default as IDCard } from '@icons/id-card.svg';
+export { default as Mail } from '@icons/mail.svg';
+export { default as MedAttention } from '@icons/medical-attention.svg';
+export { default as MedCare } from '@icons/medical-care.svg';
+export { default as OnlineMeeting } from '@icons/online-meeting.svg';
+export { default as Phone } from '@icons/phone.svg';
+export { default as Price } from '@icons/price.svg';
+export { default as Female } from '@icons/female.svg';
+export { default as Male } from '@icons/male.svg';
+export { default as Dot } from '@icons/card-dot.svg';
+export { default as BlueFb } from '@icons/specialist-facebook.svg';
+export { default as BlueInsta } from '@icons/specialist-instagram.svg';
+export { default as BlueTikTok } from '@icons/specialist-tiktok.svg';
+export { default as BlueYoutube } from '@icons/specialist-youtube.svg';
+export { default as BlueLinkedin } from '@icons/specialist-linkedin.svg';
+export { default as BlueViber } from '@icons/specialist-viber.svg';
+export { default as BlueTelegram } from '@icons/specialist-telegram.svg';
+export { default as Facebook } from '@icons/facebook.svg';
+export { default as WhiteLogo } from '@icons/whiteLogo.svg';
+export { default as CloseIcon } from '@icons/closeIcon.svg';
+export { default as BurgerIcon } from '@icons/burger.svg';
+export { default as Instagram } from '@icons/instagram.svg';
+export { default as HeaderCloseIcon } from '@icons/headerCloseIcon.svg';
+export { default as Logo } from '@icons/logo.svg';
