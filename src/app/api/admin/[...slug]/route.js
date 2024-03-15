4d1523832780ba1b6865a684d5f67d0093a59aff
BigTako <libert761@gmail.com>
2024-03-15 22:25:26 +0200

    [MID-82] fixed admin route

--- src/app/api/admin/[...slug]/route.js
+++ src/app/api/admin/[...slug]/route.js
@@ -4,78 +4,7 @@ import { prisma } from '@/lib/db';
 import { auth } from '@/lib/auth';
 import { withErrorHandler } from '@/lib/errors/errorHandler';
 import { NotAuthorizedException } from '@/lib/errors/NotAuthorizedException';
-import { RESOURCES } from '@/app/(admin)/admin/_lib/consts';
-
-const MODEL_SEARCH_FIELDS = {
-  [RESOURCES.event]: ['title', 'organizerName'],
-  [RESOURCES.specialist]: ['firstName', 'lastName', 'surname'],
-  [RESOURCES.organization]: ['name'],
-};
-
-const MODEL_INCLUDES = {
-  [RESOURCES.specialist]: {
-    therapiesCuts: { select: { id: true, therapy: { select: { id: true, title: true, type: true } }, requests: true } },
-    specializations: { select: { id: true, name: true } },
-    addresses: {
-      select: {
-        id: true,
-        nameOfClinic: true,
-        fullAddress: true,
-        district: { select: { id: true, name: true } },
-      },
-    },
-  },
-  [RESOURCES.organization]: {
-    therapiesCuts: { select: { id: true, therapy: { select: { id: true, title: true, type: true } }, requests: true } },
-    type: { select: { id: true, name: true } },
-    addresses: {
-      select: {
-        id: true,
-        nameOfClinic: true,
-        fullAddress: true,
-        district: { select: { id: true, name: true } },
-      },
-    },
-  },
-  [RESOURCES.event]: {
-    additionalLink: { select: { label: true, link: true } },
-    tags: { select: { name: true } },
-  },
-  [RESOURCES.therapy]: {
-    requests: { select: { id: true, name: true } },
-    _count: {
-      select: { requests: true },
-    },
-  },
-};
-
-function searchInputFilters(modelName, filter) {
-  if (!filter) return {};
-  const filters = MODEL_SEARCH_FIELDS[modelName].map(field => ({ [field]: { contains: filter, mode: 'insensitive' } }));
-  return { OR: filters };
-}
-
-/* eslint-disable no-param-reassign */
-function transformServiceProvider(instance, modelName) {
-  // ReferenceInput doesn't see included fields if it returned as new object, so we need to transform current
-  // React Admin issues
-  if (modelName === RESOURCES.organization) {
-    instance.organizationTypesIds = instance.type.map(orgType => orgType.id);
-  } else {
-    instance.specializationsIds = instance.specializations.map(specialization => specialization.id);
-  }
-  instance.therapiesCutsIds = instance.therapiesCuts.map(cut => cut.id);
-  instance.therapiesCuts = instance?.therapiesCuts?.map(cut => ({
-    ...cut,
-    therapyId: cut.therapy.id,
-    requestsIds: cut.requests.map(request => request.id),
-  }));
-  instance.addresses = instance?.addresses?.map(address => ({
-    ...address,
-    districtId: address.district.id,
-  }));
-}
-/* eslint-enable no-param-reassign */
+import { MODEL_INCLUDES, searchInputFilters, transformServiceProvider } from '../common';
 
 const handler = auth(
   withErrorHandler(async req => {
@@ -111,6 +40,7 @@ const handler = auth(
           specializations: true,
           therapies: true,
           therapiesCuts: true,
+          requests: true,
           type: true,
         },
         include: MODEL_INCLUDES[modelName],
