a728a3781cf5989d16b63cdf0dfa2fa9735aa857
BigTako <libert761@gmail.com>
2024-03-25 18:07:24 +0200

    [MID-114] added map to extended cards

--- src/app/_components/CardSpecialist/Organization/ExtendedCard.js
+++ src/app/_components/CardSpecialist/Organization/ExtendedCard.js
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
+import { Map } from '@components/Map';
 import { CardOrganization } from '@components/CardSpecialist/Organization/Card';
 import { CardModalWrapper } from '@components/CardSpecialist/CardModalWrapper';
 import { organizationPropType } from '@/app/_components/CardSpecialist/prop-types';
 
 export function CardOrganizationExtended({ organization, className }) {
+  const addressesPoints = organization.addresses.map(({ fullAddress, latitude, longitude }) => ({
+    title: fullAddress,
+    latitude,
+    longitude,
+  }));
+  const center = [addressesPoints[0].latitude, addressesPoints[1].longitude];
   return (
     <CardModalWrapper className="mt-[40px] w-full max-w-[1080px] lg:mt-0 lg:w-[1080px]" key={organization?.id}>
-      <CardOrganization organization={organization} className={className} extended />
+      <div className="flex flex-col gap-4">
+        <CardOrganization organization={organization} className={className} extended />
+        <div className="h-[198px] w-full md:h-[232px]">
+          <Map points={addressesPoints} center={center} zoom={13} />
+        </div>
+      </div>
     </CardModalWrapper>
   );
 }
--- src/app/_components/CardSpecialist/Specialist/ExtendedCard.js
+++ src/app/_components/CardSpecialist/Specialist/ExtendedCard.js
@@ -1,12 +1,25 @@
 import PropTypes from 'prop-types';
+import { Map } from '@components/Map';
 import { CardSpecialist } from '@components/CardSpecialist/Specialist/Card';
 import { CardModalWrapper } from '@components/CardSpecialist/CardModalWrapper';
 import { specialistPropType } from '@components/CardSpecialist/prop-types';
 
 export function CardSpecialistExtended({ specialist, className }) {
+  const addressesPoints = specialist.addresses.map(({ fullAddress, latitude, longitude }) => ({
+    title: fullAddress,
+    latitude,
+    longitude,
+  }));
+  const center = [addressesPoints[0].latitude, addressesPoints[1].longitude];
+
   return (
     <CardModalWrapper className="mt-[40px] w-full max-w-[1080px] lg:mt-0 lg:w-[1080px]" key={specialist.id}>
-      <CardSpecialist specialist={specialist} className={className} extended />
+      <div className="flex flex-col gap-4">
+        <CardSpecialist specialist={specialist} className={className} extended />
+        <div className="h-[198px] w-full md:h-[232px]">
+          <Map points={addressesPoints} center={center} zoom={13} />
+        </div>
+      </div>
     </CardModalWrapper>
   );
 }
