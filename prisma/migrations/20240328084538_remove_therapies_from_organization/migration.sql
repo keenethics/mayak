929193c71af4af04bee4c0883d480d1b476f65aa
IvanKoshan2004 <ivan.koshangod@gmail.com>
2024-03-28 10:52:00 +0200

    [MID-151] actually remove therapy from organization

--- prisma/migrations/20240328084538_remove_therapies_from_organization/migration.sql
+++ prisma/migrations/20240328084538_remove_therapies_from_organization/migration.sql
@@ -10,11 +10,5 @@ ALTER TABLE "_OrganizationToTherapy" DROP CONSTRAINT "_OrganizationToTherapy_A_f
 -- DropForeignKey
 ALTER TABLE "_OrganizationToTherapy" DROP CONSTRAINT "_OrganizationToTherapy_B_fkey";
 
--- AlterTable
-ALTER TABLE "organization" ADD COLUMN     "therapyId" UUID;
-
 -- DropTable
 DROP TABLE "_OrganizationToTherapy";
-
--- AddForeignKey
-ALTER TABLE "organization" ADD CONSTRAINT "organization_therapyId_fkey" FOREIGN KEY ("therapyId") REFERENCES "therapy"("id") ON DELETE SET NULL ON UPDATE CASCADE;
